98abcb0a8239c37fd8d4abc8c7b03fddca4cf1dd
Ivan Zotov <git@ivanzotov.com>
2023-07-13 15:40:17 +0300

    part 1

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,61 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-white dark:bg-black'>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function () {
+                  function setTheme(newTheme) {
+                    window.__theme = newTheme;
+                    if (newTheme === 'dark') {
+                      document.documentElement.classList.add('dark');
+                    } else if (newTheme === 'light') {
+                      document.documentElement.classList.remove('dark');
+                    }
+                  }
+
+                  var darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+                  window.__preferredTheme = 'auto';
+                  try {
+                    var preferredTheme = localStorage.getItem('theme');
+                    if (preferredTheme) {
+                      window.__preferredTheme = preferredTheme;
+                    }
+                  } catch (err) { }
+
+                  window.__setPreferredTheme = function(newTheme) {
+                    window.__preferredTheme = newTheme;
+                    if (newTheme === 'auto') {
+                      setTheme(darkQuery.matches ? 'dark' : 'light');
+                      try {
+                        localStorage.removeItem('theme');
+                      } catch (err) { }
+                    } else {
+                      setTheme(newTheme);
+                      try {
+                        localStorage.setItem('theme', newTheme);
+                      } catch (err) { }
+                    }
+                  };
+
+                  var initialTheme = window.__preferredTheme;
+
+                  if (initialTheme === 'auto') {
+                    initialTheme = darkQuery.matches ? 'dark' : 'light';
+                  }
+                  setTheme(initialTheme);
+
+                  darkQuery.addEventListener('change', function (e) {
+                    if (window.__preferredTheme === 'auto') {
+                      setTheme(e.matches ? 'dark' : 'light');
+                    }
+                  });
+                })();
+              `,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
